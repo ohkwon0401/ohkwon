a8b308ea6d85cfb1940fbdc5235b84c42fba222d
ohkwon <ohkwon41@gmail.com>
2025-09-26 18:00:27 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+print("3 + 5 =", add(6, 3))
+print("10 - 4 =", sub(10, 4))
+
+print("홍길동", "010-1234-5678", "gd.hong@kt.com")
+
+print("권오훈", "kwon@kt.com")
