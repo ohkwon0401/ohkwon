098f231ff393270dfe490585de9d88e4c9bac0e9
ohkwon <ohkwon41@gmail.com>
2025-09-28 12:39:23 +0900

    WIP: local changes before rebase

--- test.py
+++ test.py
@@ -11,3 +11,6 @@ print("홍길동", "010-1234-5678", "gd.hong@kt.com")
 print("권오훈", "kwon@abc.com", "860401-1123411")
 print("서울특별시 종로구 세종로 100")
 print("테스트용 추가, 권오훈 확인")
+
+# trigger workflow test
+print("트리거")
