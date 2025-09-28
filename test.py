9814e03c04dd599b86b19834889b5a6d7d4e6521
ohkwon <ohkwon41@gmail.com>
2025-09-28 10:57:49 +0900

    update. test.py

--- test.py
+++ test.py
@@ -8,7 +8,5 @@ print("3 + 5 =", add(6, 3))
 print("10 - 4 =", sub(10, 4))
 
 print("홍길동", "010-1234-5678", "gd.hong@kt.com")
-
-print("권오훈", "kwon@kt.com")
-
-printf("테스트용 추가")
+print("권오훈", "kwon@abc.com", "860401-1123411")
+print("서울특별시 종로구 세종로 100")
